ed0f499f613c0e06a5e4599103d3c09162437f29
Kevin Ukota <z1ppm4n@gmail.com>
2023-11-26 22:02:05 +0100

    Create playRound function and correct typo
    Create function playRound() that plays a single round of Rock Paper Scissors by taking two parameters, playerSelection and computerSelection and returns a corresponding string by using a switch statement. Also correct typo from "scizzors" to "scissors".

--- javascript.js
+++ javascript.js
@@ -10,7 +10,32 @@ function getComputerChoice() {
       return "Paper";
       break;
     case 3:
-      return "Scizzors";
+      return "Scissors";
       break;
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  let playerChoice =
+    playerSelection.charAt(0).toUpperCase() +
+    playerSelection.slice(1).toLowerCase();
+  let computerChoice = computerSelection;
+  switch (true) {
+    case (playerChoice === "Rock" && computerChoice === "Rock") ||
+      (playerChoice === "Paper" && computerChoice === "Paper") ||
+      (playerChoice === "Scissors" && computerChoice === "Scissors"):
+      return "It's a tie!";
+      break;
+    case (playerChoice === "Rock" && computerChoice === "Scissors") ||
+      (playerChoice === "Paper" && computerChoice === "Rock") ||
+      (playerChoice === "Scissors" && computerChoice === "Paper"):
+      return `You Win! ${playerChoice} beats ${computerChoice}`;
+      break;
+    case (playerChoice === "Rock" && computerChoice === "Paper") ||
+      (playerChoice === "Paper" && computerChoice === "Scissors") ||
+      (playerChoice === "Scissors" && computerChoice === "Rock"):
+      return `You Lose! ${playerChoice} beats ${computerChoice}`;
+      break;
+  }
+}
+console.log(playRound("rOcK", getComputerChoice()));
